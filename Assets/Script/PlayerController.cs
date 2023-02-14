7423c3734bc5e68758797182e260ae8e01b41d30
JH-Chong <88106151+JH-Chong@users.noreply.github.com>
2023-02-14 16:38:31 +0800

    Wrong collision update

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -2,22 +2,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
-    private const float SPEED = 5.0f;
+    // Variables affecting the player character m_Movement
+    private const float m_SPEED = 5.0f;
 
-    private Camera          mainCamera;
-    private CameraSettings  cameraSettings;
+    // Variables affecting the player character animation
+    private Animator m_Animator;
+    private Vector2 m_Movement;
 
-    public Animator animator;
-
-    Vector2 movement;
+    // Main camera and its components
+    private Camera          m_MainCamera;
+    private CameraSettings  m_CameraSettings;
 
     void Start()
     {
-        mainCamera = Camera.main;
-        cameraSettings = mainCamera.GetComponent<CameraSettings>();
+        m_MainCamera = Camera.main;
+        m_CameraSettings = m_MainCamera.GetComponent<CameraSettings>();
 
-        animator = this.GetComponentInChildren<Animator>();
+        m_Animator = gameObject.GetComponentInChildren<Animator>();
     }
 
     void MovePlayerOnWASDPressed()
@@ -25,25 +26,27 @@ public class PlayerController : MonoBehaviour
         float keyWS = Input.GetAxis("Vertical");
         float keyAD = Input.GetAxis("Horizontal");
 
-        movement.x = keyWS;
-        movement.y = keyAD;
+        m_Movement.x = keyWS;
+        m_Movement.y = keyAD;
 
-        animator.SetFloat("MoveX", movement.x);
-        animator.SetFloat("MoveY", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
+        if (m_Movement.x != 0)
+            m_Animator.SetFloat("MoveX", m_Movement.x);
+        if (m_Movement.y != 0)
+            m_Animator.SetFloat("MoveY", m_Movement.y);
+        m_Animator.SetFloat("Speed", m_Movement.sqrMagnitude);
 
         // The translations are relative to the x-z plane
-        float translateX = SPEED * keyWS * Time.deltaTime;
-        float translateZ = SPEED * keyAD * Time.deltaTime;
+        float translateX = m_SPEED * keyWS * Time.deltaTime;
+        float translateZ = m_SPEED * keyAD * Time.deltaTime;
 
         // Move the player
         transform.Translate(translateZ, 0.0f, translateX);
 
         // Move the camera and adjust its look/forward vector
-        mainCamera.transform.position = transform.position + new Vector3(cameraSettings.xOffsetFromPlayer, 
-                                                                         cameraSettings.yOffsetFromPlayer,
-                                                                         cameraSettings.zOffsetFromPlayer);
-        mainCamera.transform.LookAt(transform);
+        m_MainCamera.transform.position = transform.position + new Vector3(m_CameraSettings.xOffsetFromPlayer, 
+                                                                         m_CameraSettings.yOffsetFromPlayer,
+                                                                         m_CameraSettings.zOffsetFromPlayer);
+        m_MainCamera.transform.LookAt(transform);
     }
 
     void Update()
@@ -51,20 +54,13 @@ public class PlayerController : MonoBehaviour
         MovePlayerOnWASDPressed();
     }
 
-    void ConstraintAxisMovement(Collision other)
+    void OnCollisionEnter(Collision collision)
     {
-        Transform otherObjTransform = other.collider.transform;
-        
+        Debug.Log("Collision Enter");
     }
 
-    void OnCollisionEnter(Collision other)
+    void OnTriggerEnter(Collider collider)
     {
-        Debug.Log("ColEnter");
-        ConstraintAxisMovement(other);
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("TriEnter");
+        Debug.Log("Trigger Enter");
     }
 }
