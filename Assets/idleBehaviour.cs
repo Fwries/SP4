3a6784d569cc769eb408ae5417068f6661d7c78c
SquishableRTWW <SquishableRTWW@gmail.com>
2023-02-15 09:08:36 +0800

    working on enemy AI, gave enemy a tag

--- Assets/idleBehaviour.cs
+++ Assets/idleBehaviour.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class idleBehaviour : StateMachineBehaviour
 {
     float timer;
+    Transform myTransform;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         timer = 0;
+        myTransform = animator.GetComponent<Transform>();
+        myTransform = myTransform;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
