58774eeae88c4783252a38b5a6a33615d7bbfe56
Fwries <totallynotforfaries@gmail.com>
2023-02-15 15:22:45 +0800

    Sword Rotate

--- Assets/Script/WeaponBehaviour.cs
+++ Assets/Script/WeaponBehaviour.cs
@@ -14,11 +14,12 @@ public class WeaponBehaviour : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, layerMask))
         {
-            transform.position = raycastHit.point;
+            //transform.position = raycastHit.point;
+            dir = new Vector3(raycastHit.point.x - transform.position.x, raycastHit.point.y - transform.position.y, raycastHit.point.z - transform.position.z);
         }
 
-        //angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        //Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.back);
-        //transform.rotation = rotation;
+        angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle - 90, new Vector3(1, 0, 0));
+        transform.rotation = rotation;
     }
 }
