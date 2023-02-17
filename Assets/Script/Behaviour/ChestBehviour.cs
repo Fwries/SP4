418737f6e46f03f362d7402cf662c9a06165140b
SquishableRTWW <SquishableRTWW@gmail.com>
2023-02-17 23:16:05 +0800

    Added chest object (using cube as placeholder for now, dops only coins)

---
+++ Assets/Script/Behaviour/ChestBehviour.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestBehviour : MonoBehaviour
+{
+    public Transform coinPos;
+    private Vector3 coinOff = new Vector3(0f, 0f, -1.2f);
+
+    private GameObject player;
+    public GameObject prefabToSpawn;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player = null;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && player != null)
+        {
+            Transform coin = Instantiate(coinPos, transform.position + coinOff, Quaternion.identity);
+            coin.GetComponent<coinBehaviour>().SetUp(Random.Range(1, 10));
+        }
+    }
+}
