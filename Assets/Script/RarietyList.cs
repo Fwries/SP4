fce0e7336ba30adf8e88e306f1e8f94ed5a8ab70
Fwries <totallynotforfaries@gmail.com>
2023-02-17 20:35:05 +0800

    Changed RarietyList

---
+++ Assets/Script/RarietyList.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RarietyList : MonoBehaviour
+{
+    public GameObject[] Common;
+    public GameObject[] Rare;
+    public GameObject[] Epic;
+    public GameObject[] Legendary;
+
+    public GameObject GetRandomGameObject()
+    {
+        int RandRariety = Random.Range(0, 101);
+        if (RandRariety < 51)
+        {
+            return Common[Random.Range(0, Common.Length)];
+        }
+        else if (RandRariety < 81)
+        {
+            return Rare[Random.Range(0, Common.Length)];
+        }
+        else if (RandRariety < 96)
+        {
+            return Epic[Random.Range(0, Common.Length)];
+        }
+        else if (RandRariety < 101)
+        {
+            return Legendary[Random.Range(0, Common.Length)];
+        }
+        return null;
+    }
+}
