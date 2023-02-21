f30af437cce5dd69073f550781721f6758e29e19
SquishableRTWW <SquishableRTWW@gmail.com>
2023-02-21 22:32:18 +0800

    Added more effects to the bullet (cos I can)

--- Assets/Script/Behaviour/MagicBallBehaviour.cs
+++ Assets/Script/Behaviour/MagicBallBehaviour.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class MagicBallBehaviour : MonoBehaviour
 {
     public GameObject explosionParticles;
+    public GameObject projParticles1;
+    public GameObject projParticlesReal;
     public GameObject player;
     private Vector3 shootDir;
     Color originalColor;
     private int damage;
     float projSpeed;
+    private void Awake()
+    {
+        projParticlesReal = Instantiate(projParticles1, transform.position, Quaternion.identity);
+        projParticlesReal.GetComponent<Transform>().SetParent(this.transform);
+    }
+
     public void SetUp(Vector3 shootDirection, int Damage, float ProjSpeed)
     {
         shootDir = shootDirection;
@@ -22,6 +30,7 @@ public class MagicBallBehaviour : MonoBehaviour
         transform.position += shootDir * projSpeed * Time.deltaTime;
         if (transform.position.y < -0.1)
         {
+            projParticlesReal.GetComponent<ParticleSystem>().Stop();
             Destroy(gameObject);
         }
     }
@@ -30,7 +39,7 @@ public class MagicBallBehaviour : MonoBehaviour
     {
         Instantiate(explosionParticles, transform.position, Quaternion.identity);
 
-
+        projParticlesReal.GetComponent<ParticleSystem>().Stop();
         Destroy(gameObject);
         if (collision.gameObject.tag == "Player")
         {
