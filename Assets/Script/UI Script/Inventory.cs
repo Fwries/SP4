e065a3da243a3941d601845e14f8f0beb46e01a3
Cedric <yeojunwen@gmail.com>
2023-02-21 15:14:46 +0800

    Inventory changes icon

---
+++ Assets/Script/UI Script/Inventory.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+	#region Singleton
+
+	public static Inventory instance;
+
+	void Awake()
+	{
+		if (instance != null)
+		{
+			Debug.LogWarning("More than one instance of Inventory found!");
+			return;
+		}
+
+		instance = this;
+	}
+
+	#endregion
+
+	// Callback which is triggered when
+	// an item gets added/removed.
+	public delegate void OnItemChanged();
+	public OnItemChanged onItemChangedCallback;
+
+	public int space = 9;  // Amount of slots in inventory
+
+	// Current list of items in inventory
+	public List<ScEquipment> items = new List<ScEquipment>();
+
+	// Add a new item. If there is enough room we
+	// return true. Else we return false.
+	public bool Add(ScEquipment item)
+	{
+		
+		// Check if out of space
+		if (items.Count >= space)
+		{
+			Debug.Log("Not enough room.");
+			return false;
+		}
+
+		items.Add(item);    // Add item to list
+
+		// Trigger callback
+		if (onItemChangedCallback != null)
+			onItemChangedCallback.Invoke();
+
+		return true;
+	}
+
+}
---
+++ Assets/Script/UI Script/InventoryUI.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    public Transform equipmentParent;
+    EquipmentSlot[] slots;
+    Inventory inventory;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = Inventory.instance;
+        inventory.onItemChangedCallback += UpdateUI;
+        slots = equipmentParent.GetComponentsInChildren<EquipmentSlot>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateUI()
+    {
+        for(int i = 0; i<slots.Length;i++)
+        {
+            if(i<inventory.items.Count)
+            {
+                slots[i].AddItem(inventory.items[i]);
+            }
+            else
+            {
+                slots[i].ClearSlot();
+            }
+        }
+    }
+}
