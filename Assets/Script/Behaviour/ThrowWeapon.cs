d297a50d16de62659007729835c07954fa1764d5
Fwries <totallynotforfaries@gmail.com>
2023-02-21 10:08:08 +0800

    Fixed Weapon again

--- Assets/Script/Behaviour/ThrowWeapon.cs
+++ Assets/Script/Behaviour/ThrowWeapon.cs
@@ -31,7 +31,7 @@ public class ThrowWeapon : MonoBehaviour
         // We take the mass into account to slow down the rotation speed
         m_RotateSpeed = 3600 - (180 * scWeapon.Mass);
         // We want take the weapon reach into account
-        m_WeaponTravelDist = 5.0f * scWeapon.AtkReach;
+        m_WeaponTravelDist = 3.0f * scWeapon.AtkReach;
 
         // Unparent the weapon, turn it into a top-level object in the hierarchy
         transform.SetParent(null);
