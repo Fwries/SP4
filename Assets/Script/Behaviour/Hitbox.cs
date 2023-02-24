5dd8cf6adc4809f7e256f5ccc68e6eb1cdf5c815
Fwries <totallynotforfaries@gmail.com>
2023-02-24 18:35:54 +0800

    kepish

--- Assets/Script/Behaviour/Hitbox.cs
+++ Assets/Script/Behaviour/Hitbox.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class Hitbox : MonoBehaviour
 {
@@ -9,7 +10,14 @@ public class Hitbox : MonoBehaviour
     public int Damage;
     public bool active;
 
+    [SerializeField] private GameObject Weapon;
     [SerializeField] private HitboxContainer hitBoxContainer;
+    private PhotonView photonview;
+
+    void Awake()
+    {
+        photonview = Weapon.GetComponent<PhotonView>();
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -19,20 +27,35 @@ public class Hitbox : MonoBehaviour
             Animator otherAnim = other.gameObject.GetComponent<Animator>();
             if (!otherAnim.GetBool("IsHit"))
             {
-                otherAnim.SetBool("IsHit", true);
-                int Health = otherAnim.GetInteger("Health");
-
-                otherAnim.SetInteger("PrevHealth", otherAnim.GetInteger("Health"));
-                otherAnim.SetInteger("Health", Health - (Damage * Crit));
-
+                Debug.Log(other.gameObject.GetComponent<PhotonView>().ViewID);
+                photonview.GetComponent<PhotonView>().RPC("RPC_Damage", RpcTarget.All, other.gameObject.GetComponent<PhotonView>().ViewID, Damage * Crit);
                 hitBoxContainer.OnHit(index);
             }
         }
-        if(other.gameObject.tag=="BreakWall")
+        if (other.gameObject.tag == "BreakWall")
         {
-            Destroy(other.gameObject);
             hitBoxContainer.OnHit(index);
-            hitBoxContainer.DestroyWeapon();
+            photonview.GetComponent<PhotonView>().RPC("RPC_WallBreak", RpcTarget.Others, other.gameObject.GetComponent<PhotonView>().ViewID, Weapon.GetComponent<PhotonView>().ViewID);
         }
+    }
+
+    [PunRPC]
+    void RPC_Damage(int view_id, int _Damage)
+    {
+        Animator otherAnim = PhotonView.Find(view_id).gameObject.GetComponent<Animator>();
+
+        otherAnim.SetBool("IsHit", true);
+        int Health = otherAnim.GetInteger("Health");
+
+        otherAnim.SetInteger("PrevHealth", otherAnim.GetInteger("Health"));
+        otherAnim.SetInteger("Health", Health - _Damage);
+    }
+
+    [PunRPC]
+    void RPC_WallBreak(int view_id, int Weapon_id)
+    {
+        GameObject Wall = PhotonView.Find(view_id).gameObject;
+        Destroy(Wall);
+        PhotonView.Find(Weapon_id).gameObject.GetComponent<HitboxContainer>().DestroyWeapon();
     }
 }
